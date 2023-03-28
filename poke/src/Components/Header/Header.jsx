1f21e2d821c9e2a1fe074baf7b5c5ca09dd7084a
Rajesh-Reel <rajreel2@hotmail.com>
2023-03-28 18:33:00 +0100

    changed background css to aqua and sorted image size

--- poke/src/Components/Header/Header.jsx
+++ poke/src/Components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import pokemonLogo from "../../assets/pokemon-logo.png";
+import "./Header.css";
 
 function Header() {
     return(
-        <img className={'Logo'} src={pokemonLogo} alt="pokemonLogo"></img>
+        <img className="logo" src={pokemonLogo} alt="pokemonLogo"></img>
     )
 }
 
